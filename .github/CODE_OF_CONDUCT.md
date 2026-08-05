7f1b8e658d82927a16eef48ce52c3cdea1436eaf
OverYandere <naomikimmey@gmail.com>
2026-08-05 17:49:42 -0400

    Set up

---
+++ .github/CODE_OF_CONDUCT.md
@@ -0,0 +1,4 @@
+# Contributor Code of Conduct
+
+1. Do not contribute malicious code
+2. Do not contribute anything which is illegal in the United States
---
+++ mod.cpp
@@ -0,0 +1,7 @@
+name = "Bzn6thHalo Aux";
+picture = "logo.paa";
+overview = "An aux mod for the Buzan 6th Company Unit.";
+logo = "logo.paa";
+logoOver = "logo.paa";
+actionName = "GitHub";
+action = "";
